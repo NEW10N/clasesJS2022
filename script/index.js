0ec57a6edb2989a5da172472122040a78dab3675
Cesar Castro <cesarcastro10@outlook.com>
2022-09-27 16:02:16 -0500

    Caracteristicas de las variables

---
+++ script/index.js
@@ -0,0 +1,27 @@
+// var me permite redeclarar variable con el mismo nombre y  tambien puedo sobreescribir y mutar el tipo de dato
+var nombre //Esto es declarar la variable, solamente escribirla, sin ponerle algun valor
+nombre = "Carlos" //Aqui se esta asignando un valor a la varibale
+var nombre = "Pedro"//Aqui se esta declarando la variable y se esta  inicializando la variable
+nombre = 12 //Puedo cambiar el tipo de datos
+
+
+// let 
+let apellido //Puedo declararla
+apellido = "Torres" //Puedo asignarle un valor
+apellido = "Garcia" //Puedo reasignarle un valor
+apellido = [] //Puedo cambiar el tipo de daots
+// let apellido = "Rodrgiguez" //No se puede redeclarar la variable
+
+//const
+// const edad       //No permite declararla sin inia
+const edad = 35
+// edad = 53 //No se puede reasgnar un valor y el tipo de dato
+//No se puede declarar una variable con el mismo nombre
+
+
+
+// construir un programa que me diga si me puedo jubilar
+// utilizar propt para preguntarle al usuario , su nombre, edad y genero si es mujer a los 60 y si es hombre a los 65
+// y con un alert avisar si ya esta en edad de jubilarse y si no cuantos años de aporte le faltan
+
+
