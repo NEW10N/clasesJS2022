b5d2e952900da12ba7942cfabb2fb45b58a6b918
Cesar Castro <cesarcastro10@outlook.com>
2022-09-27 18:04:50 -0500

    EjercicioJS

--- script/index.js
+++ script/index.js
@@ -1,27 +1,29 @@
-// var me permite redeclarar variable con el mismo nombre y  tambien puedo sobreescribir y mutar el tipo de dato
-var nombre //Esto es declarar la variable, solamente escribirla, sin ponerle algun valor
-nombre = "Carlos" //Aqui se esta asignando un valor a la varibale
-var nombre = "Pedro"//Aqui se esta declarando la variable y se esta  inicializando la variable
-nombre = 12 //Puedo cambiar el tipo de datos
-
-
-// let 
-let apellido //Puedo declararla
-apellido = "Torres" //Puedo asignarle un valor
-apellido = "Garcia" //Puedo reasignarle un valor
-apellido = [] //Puedo cambiar el tipo de daots
-// let apellido = "Rodrgiguez" //No se puede redeclarar la variable
-
-//const
-// const edad       //No permite declararla sin inia
-const edad = 35
-// edad = 53 //No se puede reasgnar un valor y el tipo de dato
-//No se puede declarar una variable con el mismo nombre
-
-
 
 // construir un programa que me diga si me puedo jubilar
+
 // utilizar propt para preguntarle al usuario , su nombre, edad y genero si es mujer a los 60 y si es hombre a los 65
+
 // y con un alert avisar si ya esta en edad de jubilarse y si no cuantos años de aporte le faltan
 
 
+let nombre = prompt('Cual es tu nombre?')
+let edad = prompt('Cual es tu edad?')
+let genero = prompt('Cual es tu genero? (hombre/mujer)')
+
+if (genero.toLowerCase() === 'hombre'){
+    if (edad >= 65){
+        alert('Te puedes jubilar ' + nombre +'.')//opcion 1 para concatenar
+        console.log("Te puedes jubilar ", nombre, ".")//opcion2 para concatenar
+        alert(`Puedes jubilarte ${nombre}, felicidades!!!.`)
+    }else{
+        alert('Lo sentimos ' + nombre +', aun no puedes jubilrte, hasta dentro de ' + String(65 - parseInt(edad)) + ' anos')
+    }
+}else if(genero.toLowerCase() === 'mujer'){
+    if (edad >= 60){
+        alert('Te puedes jubilar ' + nombre +'.')
+    }else{
+        alert('Lo sentimos ' + nombre +', aun no puedes jubilrte, hasta dentro de ' + String(60 - parseInt(edad)) + ' anos')
+    }
+}else{
+    alert("No se selecciono un genero correcto")
+}
