3d85cad6c2f1b29b9ab9352f2360bf9f11ecf5e0
Cesar Castro <cesarcastro10@outlook.com>
2022-09-27 14:59:24 -0500

    Primeros pasos con javascript

---
+++ script/index.js
@@ -0,0 +1,99 @@
+// Declaracion de variables
+// Hay  tres formas de declarar variables
+var nombre 
+let apellido
+const edad = 0
+
+
+// Asignacion de variables
+nombre = "Chayanne"
+apellido = "Arce"
+
+// inicializar variables
+var mascota = "Solovino"
+
+
+
+// var permite re declarar y sobre escribir la variabele
+
+var carro
+console.log(carro)
+var carro = ""
+console.log(carro)
+var carro = "nissan"
+console.log(carro)
+
+
+let comida = "tacos"
+console.log(comida)
+comida = "chicharron de la ramos"
+console.log(comida)
+// let comida = "pollos"  //No es posible ya que tenemos inicalizada en let
+console.log("Las variables let no se pueden re inicializar")
+
+
+
+function cat(){
+    // Es muy malo declarar variables sin let o var 
+    gato = "persa"
+    console.log(gato)
+}
+// console.log(gato)
+cat()
+console.log(gato)
+gato = "persa"
+console.log(gato)
+
+
+// Variables Primitivas
+let string = "Es un conjunto de palabras"
+
+
+// datos primitivos 
+
+
+//datos complejos 
+//Objeto
+ let ferrari{   Pintura : 'roja' }
+
+
+
+ //Valores por referencia
+ let nombre = "Osvaldin"
+//  let apellido = "Castro"
+
+//  let usuario {
+//     nombre:nombre
+//     // apellido:apellido
+//  }
+
+//  usuario   
+
+
+
+
+//---------------------------------------------------------------------------------------------------------------------------------------------------------------
+// Building Methods
+console.log()
+alert("hola")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
