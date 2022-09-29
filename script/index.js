35dcaa62f753ecd001b597d23a6c9c9918e5dd4e
Cesar Castro <cesarcastro10@outlook.com>
2022-09-29 15:23:58 -0500

    Formato Inicial

---
+++ script/index.js
@@ -0,0 +1,46 @@
+// Bucles de iteraciones
+// for , while, do while
+// let a = 5
+// a*= 5
+// a
+
+//For
+// let i = 0;
+// for (; i < 5; i++) {
+//     // console.log("hola")
+//     // console.log("hola" + i + 1)
+//     console.log(`hola ${i + 1}`)
+// }
+
+// console.log(i)
+
+// for (; i < 10; i++) {
+//     // console.log("adios")
+//     // console.log("adios" + i + 1)
+//     console.log(`adios ${i + 1}`)
+// }
+
+
+//While
+// let j = 0 
+
+// while (j < 5){
+//     console.log(j)
+//     j++
+// }
+
+
+// do while
+// let z = 0
+// do{
+//     console.log(z)
+//     z++
+// }while(z <= 30)
+
+
+let y = 1
+let boolean =true
+do{
+    console.log(y)
+    y < 5? y++: boolean= false
+}while(boolean)
