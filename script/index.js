5916ebf02ff8a78a60c71a997a77060ba0132e51
Cesar Castro <cesarcastro10@outlook.com>
2022-09-30 19:03:52 -0500

    Ejemplo de estructuracion de una funcion

--- script/index.js
+++ script/index.js
@@ -1,46 +1,11 @@
-// Bucles de iteraciones
-// for , while, do while
-// let a = 5
-// a*= 5
-// a
+// Funciones en JavaScript
+//  Estructura general
 
-//For
-// let i = 0;
-// for (; i < 5; i++) {
-//     // console.log("hola")
-//     // console.log("hola" + i + 1)
-//     console.log(`hola ${i + 1}`)
-// }
-
-// console.log(i)
-
-// for (; i < 10; i++) {
-//     // console.log("adios")
-//     // console.log("adios" + i + 1)
-//     console.log(`adios ${i + 1}`)
-// }
+function saludar(){
+    console.log("Hola como estas?(funcion)");
+}
 
 
-//While
-// let j = 0 
+console.log("Hola, como estas?");
 
-// while (j < 5){
-//     console.log(j)
-//     j++
-// }
-
-
-// do while
-// let z = 0
-// do{
-//     console.log(z)
-//     z++
-// }while(z <= 30)
-
-
-let y = 1
-let boolean =true
-do{
-    console.log(y)
-    y < 5? y++: boolean= false
-}while(boolean)
+// Tarea Pasar todos los ejercicios a un catalogo de juegos
