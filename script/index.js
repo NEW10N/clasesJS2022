b85cad048ee1e477672c803d46034f94cf1e1829
Cesar Castro <cesarcastro10@outlook.com>
2022-10-26 17:07:36 -0500

    Ejercicio API assync away

--- script/index.js
+++ script/index.js
@@ -45,19 +45,7 @@ const edad = inputs[1]
 // form.addEventListener("submit",)
 form.onsubmit = (e) => {
     e.preventDefault()
-    try {
-        const nombreReplace = (nombre.value.replace(/\D/g, ''))
-        if (!nombre.value) throw "El nombre no puede estar vacio"
-        if (!edad.value) throw "La Edad no puede estar vacia"
-        if (edad.value < 18) throw "Debes ser mayor de edad"
-        if (nombreReplace != "") throw "No se pueden ingresar numeros en el nombre"
-        p.textContent = `${nombre.value} ${edad.value}`
-    } catch (e) {
-        p.textContent = e
-    } finally {
-        nombre.value = ""
-        edad.value = null
-    }
+   
 }
 
 
